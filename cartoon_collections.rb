1a98cab8dd9f897e2eaac7e9c95f9d46c6b3c6d0
Alethia Quintero <alethiaq22@gmail.com>
2019-07-16 19:31:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,9 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  array.map {|planet| planet.capitalize}
+  array.each {|planet| puts planet + '!'}# code an argument here
   # Your code here
 end
 
